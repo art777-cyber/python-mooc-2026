ea884c3b90a23aa634b334f044843a2abc6a6764
art777-cyber <aryarajtyagi777@gmail.com>
2026-06-18 14:02:05 +0900

    more exercise

--- part04-11_first_second_last/src/first_second_last.py
+++ part04-11_first_second_last/src/first_second_last.py
@@ -1,12 +1,36 @@
 def first_word(sentence):
-    index=sentence.find("o")
+    index=sentence.find(" ")
+    indexarray=[]
+    while index>0:
+        indexarray.append(index)
+        index=sentence.find(" ",index+1)
+    return(sentence[0:indexarray[0]])
+
+def second_word(sentence):
+    index=sentence.find(" ")
+    indexarray=[]
+    while index>0:
+        indexarray.append(index)
+        index=sentence.find(" ",index+1)
+    if len(indexarray)<2:
+        return(sentence[indexarray[0]+1:])
+    else:
+        return(sentence[indexarray[0]+1:indexarray[1]])
+
+def last_word(sentence):
+    index=sentence.find(" ")
+    indexarray=[]
+    while index>0:
+        indexarray.append(index)
+        index=sentence.find(" ",index+1)
+    return(sentence[indexarray[-1]+1:])
     
 
 
 
 
 if __name__ == "__main__":
-    sentence = "once upon a time there was a programmer"
+    sentence = "first second"
     print(first_word(sentence))
     print(second_word(sentence))
     print(last_word(sentence))
