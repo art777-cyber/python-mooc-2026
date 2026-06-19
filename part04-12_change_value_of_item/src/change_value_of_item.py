a48a946167caaa3e0d586a68da81405670c240ce
art777-cyber <aryarajtyagi777@gmail.com>
2026-06-19 10:02:59 +0900

    more exercses

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,10 @@
-# Write your solution here
+list=[1, 2, 3, 4, 5]
+index=0
+while True:
+    index=int(input("Index: "))
+    if index!=-1:
+       newvalue=int(input("New value: "))
+       list[index]=newvalue
+       print(list)
+    else:
+      break
