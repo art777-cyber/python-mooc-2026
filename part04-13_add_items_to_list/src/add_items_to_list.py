a46415e96dc1ef699b79842eaeac38ff628618b7
art777-cyber <aryarajtyagi777@gmail.com>
2026-06-22 02:01:47 +0900

    more exercises

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,8 @@
-# Write your solution here
+num=int(input("How many items: "))
+n=1
+list=[]
+while n<=num:
+    entry=int((input("Item " + str(n) + ": ")))
+    list.append(entry)
+    n+=1
+print(list)
--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,14 @@
-# Write your solution here
+list=[]
+print("The list is now []")
+while True:
+    action=str(input("a(d)d, (r)emove or e(x)it: "))
+    if action=="d":
+        list.append(len(list) + 1)
+        print(f"The list is now {list}")
+    elif action=="r":
+        if len(list)>0:
+            list.pop(len(list)-1)
+        print(f"The list is now {list}")    
+    elif action=="x":
+        break
+print("Bye!")
