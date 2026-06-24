b61a2006e9633ea8a45cf0165624ac00cf1c369e
art777-cyber <aryarajtyagi777@gmail.com>
2026-06-24 16:08:06 +0900

    more exercises

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,22 @@
-# Write your solution here
+list_1=[]
+list_2=[]
+list=[]
+while True:
+    entry=int(input("New item: "))
+    if entry!=0:
+        list_1.append(entry)
+        list=list_1.copy()
+        list_2=[]
+        print(f"The list now: {list_1}")
+        while True:
+            if len(list)!=0:
+                list_2.append(min(list))
+                list.remove(min(list))
+            else:
+                break
+        print(f"The list in order: {list_2}")
+    else:
+        break
+print("Bye!")
+
+
--- part04-17_length_of_list/src/length_of_list.py
+++ part04-17_length_of_list/src/length_of_list.py
@@ -1,4 +1,6 @@
-# Write your solution here
+def length(a):
+    result=len(a)
+    return result
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
