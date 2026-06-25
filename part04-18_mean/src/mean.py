ba898d9de47cae65ef67efc424ad845d504dfb3e
art777-cyber <aryarajtyagi777@gmail.com>
2026-06-25 17:34:33 +0900

    more exercises

--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -1,4 +1,8 @@
-# Write your solution here
+def mean(a):
+    length=len(a)
+    s=sum(a)
+    return s/length if length > 0 else 0
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
